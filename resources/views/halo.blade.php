502e1ae925d1c906e4982b10ffb63d7e6ae66647
andk <74753424+eastonXD@users.noreply.github.com>
2024-08-09 15:41:12 +0700

    versi1

---
+++ resources/views/halo.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Keseruan Bikin website</title>
+    </head>
+    <body>
+        <h1>Website percobaan</h1>
+        <h2>Proyek AITC 2024</h2>
+        Percobaan pertama website <b>percobaan</b>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">perkenalkan nama saya dhika </a> 
+            Fusce vitae orci molestie, luctus augue eget, semper risus. Fusce metus nisi, 
+            dignissim eu maximus et,
+        </p>
+<hr>
+        <p>
+            Morbi vestibulum est eget 
+            <u>Saya Mahasiswa S1 Sistem Infromasi UNAIR'24</u><br>
+            libero porta, eget aliquet turpis tincidunt. Proin hendrerit, nisl at mollis efficitur, dolor velit dignissim erat, sit amet tincidunt dolor quam eget tellus. 
+            <hr>Maecenas et augue nisl. Fusce ullamcorper pharetra efficitur. Donec porta enim vitae mi cursus, nec consectetur nibh venenatis.
+            Aenean bibendum arcu id arcu dignissim, eget cursus tellus faucibus. 
+        Praesent eget neque in libero semper pretium. Fusce efficitur porta mi, at 
+        congue dui pellentesque ut. Ut egestas tincidunt hendrerit. Vestibulum in nisl tortor
+        </p>
+
+        <img src="https://fakeimg.pl/350x200">
+
+    </body>
+</html>
+
+
+
